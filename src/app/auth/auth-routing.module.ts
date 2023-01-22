b18afb58dcd6bccd02523865f721257a193e908a
Sunil Kumar Majhi <msunil037@gmail.com>
2023-01-22 19:30:09 +0530

    first commit

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path:'',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    component: LoginComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AuthRoutingModule { }
